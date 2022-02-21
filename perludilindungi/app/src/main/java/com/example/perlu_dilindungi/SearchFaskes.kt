f0f3d647a3a88a33f84ec9077bf04316479e81f2
yudialfayat <ayudi3521@gmail.com>
2022-02-21 22:53:57 +0700

    Create spinner for province and city

---
+++ perludilindungi/app/src/main/java/com/example/perlu_dilindungi/SearchFaskes.kt
@@ -0,0 +1,66 @@
+package com.example.perlu_dilindungi
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SearchFaskes : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_faskes)
+
+        setupProvinceSpinner()
+        setupCitySpinner()
+    }
+
+    private fun setupCitySpinner() {
+        val citySpinner: Spinner = findViewById(R.id.city_spinner)
+
+        val testArray: Array<String> = arrayOf("Kota Bandung", "Padang", "Payakumbuh")
+
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, testArray)
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+
+        citySpinner.adapter = adapter
+        citySpinner.onItemSelectedListener = this
+    }
+
+    private fun setupProvinceSpinner() {
+        val provinceSpinner: Spinner = findViewById(R.id.province_spinner)
+
+        val testArray: Array<String> = arrayOf("Jawa Barat", "Sumatera Barat", "Jawa Timur")
+
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, testArray)
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+
+        provinceSpinner.adapter = adapter
+        provinceSpinner.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        val sel: String = parent.getItemAtPosition(pos) as String
+
+        val spinnerId = parent.id;
+        if (spinnerId == R.id.province_spinner){
+            Log.i("SpinnerProvince", "Province : $sel");
+        }
+        else if (spinnerId == R.id.city_spinner){
+            Log.i("SpinnerCity", "City : $sel");
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+        Log.i("Spinner", "Nothing Selected")
+    }
+}
