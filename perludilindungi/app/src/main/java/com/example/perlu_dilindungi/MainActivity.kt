950f6263425f9d060a1341f25ad652b5be7c23ef
yudialfayat <ayudi3521@gmail.com>
2022-02-27 23:06:25 +0700

    Convert Activity to Fragment

--- perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
+++ perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.perlu_dilindungi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchFaskesActivity : Fragment = SearchFaskesFragment()
+        replaceFragment(searchFaskesActivity)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.appLayout, fragment)
+        transaction.commit()
     }
 }
--- perludilindungi/app/src/main/java/com/example/perlu_dilindungi/fragments/FaskesListFragment.kt
+++ perludilindungi/app/src/main/java/com/example/perlu_dilindungi/fragments/FaskesListFragment.kt
@@ -10,52 +10,50 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.perlu_dilindungi.FaskesDetailActivity
-import com.example.perlu_dilindungi.MainActivity
 import com.example.perlu_dilindungi.R
 import com.example.perlu_dilindungi.adapters.FaskesListAdapter
-import com.example.perlu_dilindungi.models.FaskesDetailModel
-import com.example.perlu_dilindungi.models.FaskesModel
 import com.example.perlu_dilindungi.view_models.FaskesViewModel
 
 
 class FaskesListFragment : Fragment() {
     private lateinit var viewModel: FaskesViewModel;
+
+    private val model: FaskesViewModel by activityViewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+//        Log.i("Test", "FaskesListFragment0")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        Log.i("Test", "FaskesListFragment1")
         return inflater.inflate(R.layout.fragment_faskes_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    }
+//        Log.i("Test", "FaskesListFragment2")
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-//        Log.i("Test", (2).toString())
+
         viewModel = ViewModelProvider(requireActivity())[FaskesViewModel::class.java]
-        viewModel.faskeses.observe(requireActivity(), Observer{
+        model.faskeses.observe(viewLifecycleOwner, Observer{
             if (it != null){
+                Log.i("Test", "FaskesListFragment Faskes is not null")
                 val faskesListAdapter = FaskesListAdapter(context as Activity, it)
-                val faskesListView : ListView? = view?.findViewById(R.id.faskesListView)
+                val faskesListView : ListView = view.findViewById(R.id.faskesListView)
 
-                if (faskesListView != null) {
-                    faskesListView.adapter = faskesListAdapter
-                }
+                faskesListView.adapter = faskesListAdapter
 
-                faskesListView?.onItemClickListener =
-                    OnItemClickListener { parent, view, position, id ->
-                        val i = Intent(getContext(), FaskesDetailActivity::class.java)
+                faskesListView.onItemClickListener =
+                    OnItemClickListener { _, _, position, _ ->
+                        val i = Intent(context, FaskesDetailActivity::class.java)
                         i.putExtra("id", it[position].id)
                         i.putExtra("provinsi", it[position].provinsi)
                         i.putExtra("kota", it[position].kota)
@@ -71,13 +69,17 @@ class FaskesListFragment : Fragment() {
                     }
             }
             else{
+                Log.i("Test", "FaskesListFragment Faskes is null")
                 val faskesListAdapter = FaskesListAdapter(context as Activity, ArrayList())
-                val faskesListView : ListView? = view?.findViewById(R.id.faskesListView)
-
-                if (faskesListView != null) {
-                    faskesListView.adapter = faskesListAdapter
-                }
+                val faskesListView : ListView = view.findViewById(R.id.faskesListView)
+                faskesListView.adapter = faskesListAdapter
             }
         })
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+//        Log.i("Test", "FaskesListFragment3")
     }
 }
