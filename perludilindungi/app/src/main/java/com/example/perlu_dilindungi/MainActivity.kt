8d2834b0fba234ddfc30204f66e1e2cb196aeb38
yudialfayat <ayudi3521@gmail.com>
2022-02-28 00:49:06 +0700

    Create Bottom Navbar

--- perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
+++ perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.perlu_dilindungi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.perlu_dilindungi.fragments.SearchFaskesFragment
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         val searchFaskesActivity : Fragment = SearchFaskesFragment()
         replaceFragment(searchFaskesActivity)
+
+        val navigationBar: NavigationBarView = findViewById(R.id.navbarView)
+        navigationBar.setOnItemSelectedListener {
+            if (it.itemId == R.id.nav_news){
+                Log.i("Navbar", "News")
+            }
+            if (it.itemId == R.id.nav_location){
+                Log.i("Navbar", "Location")
+            }
+            if (it.itemId == R.id.nav_bookmark){
+                Log.i("Navbar", "Bookmark")
+            }
+
+            true
+        }
     }
 
     private fun replaceFragment(fragment: Fragment){
