71d2ab55573ea1bd7e56d71e05daf51765c3386c
yudialfayat <ayudi3521@gmail.com>
2022-02-28 00:54:10 +0700

    NewsFragment: Dummy

--- perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
+++ perludilindungi/app/src/main/java/com/example/perlu_dilindungi/MainActivity.kt
@@ -4,24 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import com.example.perlu_dilindungi.fragments.NewsFragment
 import com.example.perlu_dilindungi.fragments.SearchFaskesFragment
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private val searchFaskesFragment : Fragment = SearchFaskesFragment()
+    private val newsFragment : Fragment = NewsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchFaskesActivity : Fragment = SearchFaskesFragment()
-        replaceFragment(searchFaskesActivity)
+        replaceFragment(newsFragment)
 
         val navigationBar: NavigationBarView = findViewById(R.id.navbarView)
         navigationBar.setOnItemSelectedListener {
             if (it.itemId == R.id.nav_news){
-                Log.i("Navbar", "News")
+                replaceFragment(newsFragment)
             }
             if (it.itemId == R.id.nav_location){
-                Log.i("Navbar", "Location")
+                replaceFragment(searchFaskesFragment)
             }
             if (it.itemId == R.id.nav_bookmark){
                 Log.i("Navbar", "Bookmark")
